ad2234be1e1ad7588bbd75dbd332e448d0e60092
binhex <megalith01@gmail.com>
2015-05-15 11:53:56 +0100

    add in env variable to allow to run without vpn tunnel

--- apps/nobody/sabnzbd.sh
+++ apps/nobody/sabnzbd.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
-# run script to check ip is valid for tun0
-source /home/nobody/checkip.sh
+# if vpn set to "no" then don't run openvpn
+if [[ $VPN_ENABLED == "no" ]]; then
 
-echo "[info] Starting SABnzbd..."
+	echo "[info] VPN not enabled, skipping VPN tunnel local ip checks"
+
+else
+
+	echo "[info] VPN is enabled, checking VPN tunnel local ip is valid"
+
+	# run script to check ip is valid for tun0
+	source /home/nobody/checkip.sh
+
+fi
+
+echo "[info] All checks complete, starting SABnzbd..."
 
 # run sabnzbd
 /usr/sbin/python2 /opt/sabnzbd/SABnzbd.py --config-file /config --server 0.0.0.0:8080 --https 8090
