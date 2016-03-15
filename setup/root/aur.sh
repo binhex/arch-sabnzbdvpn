9450a4ba7610e773dab6fd8724c326ab54c2c825
binhex <megalith01@gmail.com>
2016-03-15 15:04:20 +0000

    prevent removal of core packages when removing base-devel

--- setup/root/aur.sh
+++ setup/root/aur.sh
@@ -9,12 +9,6 @@ aur_helper="packer"
 # define aur packages
 aur_packages="sabnzbd"
 
-# define pacman packages
-pacman_packages="base-devel git"
-
-# install required pre-reqs for makepkg
-pacman -S --needed $pacman_packages --noconfirm
-
 # create "makepkg-user" user for makepkg
 useradd -m -s /bin/bash makepkg-user
 echo -e "makepkg-password\nmakepkg-password" | passwd makepkg-user
@@ -37,8 +31,8 @@ pacman -S --needed jansson expac jshon --noconfirm
 # install app using aur helper
 su -c "$aur_helper -S $aur_packages --noconfirm" - makepkg-user
 
-# remove base devel excluding pacman (holdpkg)
-pacman -Ru $(pacman -Qgq base-devel | grep -v pacman) --noconfirm
+# remove base devel excluding useful core packages
+pacman -Ru $(pacman -Qgq base-devel | grep -v pacman | grep -v sed | grep -v grep | grep -v gzip) --noconfirm
 
 # remove git
 pacman -Ru git --noconfirm
--- setup/root/install.sh
+++ setup/root/install.sh
@@ -4,12 +4,12 @@
 set -e
 
 # define pacman packages
-pacman_packages="python2-pyopenssl python2-feedparser"
+pacman_packages="base-devel git python2-pyopenssl python2-feedparser"
 
 # install pre-reqs
 pacman -S --needed $pacman_packages --noconfirm
 
-# call aur script
+# call aur script (arch user repo)
 source /root/aur.sh
 
 # cleanup
