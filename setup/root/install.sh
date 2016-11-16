cf1d73b402ce4de715dd1e47f967013cccf5c267
binhex <megalith01@gmail.com>
2016-11-16 13:37:41 +0000

    use base with tini, remove base-devel

--- setup/root/install.sh
+++ setup/root/install.sh
@@ -19,7 +19,7 @@ find /tmp/scripts-master/ -type f -name '*.sh' -exec mv -i {} /root/  \;
 ####
 
 # define pacman packages
-pacman_packages="base-devel git python2-pyopenssl python2-feedparser p7zip"
+pacman_packages="git python2-pyopenssl python2-feedparser p7zip"
 
 # install compiled packages using pacman
 if [[ ! -z "${pacman_packages}" ]]; then
